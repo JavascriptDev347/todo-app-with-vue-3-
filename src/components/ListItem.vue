0e2c6a8f5bcfcc4e6678b472632b21f7fc2d5ded
rustam <javascriptdev347@gmail.com>
2024-01-26 09:28:28 +0500

    todo app

---
+++ src/components/ListItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <label :class="{ 'checked': isChecked }">
+        <input type="checkbox" :checked="isChecked" />
+        <slot></slot>
+    </label>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+    isChecked?: boolean | false
+}>()
+</script>
+
+<style scoped>
+label {
+ cursor: pointer;
+}
+.checked {
+ text-decoration: line-through;
+}</style>
